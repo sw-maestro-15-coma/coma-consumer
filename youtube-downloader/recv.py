6aad383d4122f2b9c2efd54da44dacc778d58629
developerryou <developerryou@gmail.com>
2024-08-27 18:01:22 +0900

    FIX: s3 업로드 기능 개발

--- youtube-downloader/recv.py
+++ youtube-downloader/recv.py
@@ -1,5 +1,7 @@
-import pika, json
+import pika, json, time
 import utils
+
+bucket_name = 'video-process-test-bucket'
 
 def callback(ch, method, properties, body):
     data = json.loads(body.decode('utf-8'))
@@ -8,8 +10,15 @@ def callback(ch, method, properties, body):
 
     youtube_url = data.get('youtubeUrl')
     if youtube_url:
-        utils.youtube_download(youtube_url)
-        
+        youtube_key = youtube_url.split('=')[-1]
+        file_name = utils.youtube_download(youtube_url)
+        print(f"[youtube-downloader] video downloaded: {file_name}")
+
+        duration = utils.get_duration(file_name)
+        print(f"[youtube-downloader] video duration: {duration}")
+
+        s3_key = f'origin/{youtube_key}.{file_name.split(".")[-1]}'
+        utils.upload_to_s3(file_name, bucket_name, s3_key)
     else:
         print(f"[youtube-downloader] youtubeUrl not found in message")
 
