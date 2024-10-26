d68ba457eea17d558953f6e1a2ada986e3f72196
developerryou <developerryou@gmail.com>
2024-10-27 00:31:14 +0900

    FEAT: api 연결

--- youtube-downloader/main.py
+++ youtube-downloader/main.py
@@ -1,16 +1,32 @@
-import recv, sys, os
+import recv, os
+import logging
+from threading import Thread
 from dotenv import load_dotenv
 
-if __name__ == '__main__':
-    print('[youtube-downloader] Starting consumer...')
+import uvicorn
+from fastapi import FastAPI
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("main")
+logger.setLevel(logging.INFO)
+
+def start():
+    logger.info('Starting "youtube-downloader" consumer...')
     load_dotenv()
     host = os.environ.get('HOST')
-    try:
-        recv.consume(host)
-    except KeyboardInterrupt:
-        print('Interrupted')
+    while True:
         try:
-            sys.exit(0)
-        except SystemExit:
-            os._exit(0)
+            recv.consume(host)
+        except Exception as e:
+            logger.error(f"에러 발생: {e}")
 
+app = FastAPI()
+
+@app.get("/healthcheck")
+def health_check():
+    return {"message": "ok"}
+
+if __name__ == "__main__":
+    consumer_thread = Thread(target=start)
+    consumer_thread.start()
+    uvicorn.run(app, host="0.0.0.0", port=8000)
--- youtube-downloader/utils.py
+++ youtube-downloader/utils.py
@@ -1,23 +1,35 @@
-import yt_dlp, boto3, cv2, json
- 
+import yt_dlp, boto3, cv2, json, requests
+import logging
+
+logger = logging.getLogger("utils")
+logger.setLevel(logging.INFO)
+
+def get_video_title(youtube_url):
+    ydl_opts = {}
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        result = ydl.extract_info(youtube_url, download=False)
+        return result.get('title')
+
 def youtube_download(youtube_url):
-    try:
-        ydl_opts = {}
-        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            result = ydl.extract_info(youtube_url, download=True)
-            return ydl.prepare_filename(result)
-    except Exception as e:
-        print(f"[youtube-downloader] {e}")
+    ydl_opts = {}
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        result = ydl.extract_info(youtube_url, download=True)
+        return ydl.prepare_filename(result)
 
 
 def upload_to_s3(file_name, bucket_name, s3_key):
-    try:
-        s3 = boto3.client('s3')
-        s3.upload_file(file_name, bucket_name, s3_key)
-        print(f"[youtube-downloader] video uploaded {file_name} to s3://{bucket_name}/{s3_key}")
-    except Exception as e:
-        print(f"[youtube-downloader] {e}")
+    s3 = boto3.client('s3')
+    s3.upload_file(file_name, bucket_name, s3_key)
 
 def get_duration(filename):
     video = cv2.VideoCapture(filename)
     return video.get(cv2.CAP_PROP_FRAME_COUNT) / video.get(cv2.CAP_PROP_FPS)
+
+def post_success_to_api(data):
+    url = "https://api.cotuber.com/api/v1/message/video"
+    headers = {"Content-Type": "application/json"}
+    response = requests.post(url, data=json.dumps(data), headers=headers)
+    return response
+
+def post_fail_to_api(data):
+    pass
