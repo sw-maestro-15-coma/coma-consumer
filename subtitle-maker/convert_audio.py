8060538af272c945c30c29912249662a32bfd125
Octoping <mooner925@gmail.com>
2024-10-30 19:43:50 +0900

    refactor: ffmpeg에 y옵션 추가

--- subtitle-maker/convert_audio.py
+++ subtitle-maker/convert_audio.py
@@ -4,7 +4,10 @@ import ffmpeg
 
 def convert_audio(video_id: int, video_path: str) -> str:
     audio_path = f"{os.environ.get("audio_path")}/{video_id}.mp3"
-    ffmpeg.input(video_path).output(audio_path).run()
+
+    ffmpeg.input(video_path)\
+        .output(audio_path, y='-y')\
+        .run()
 
     return audio_path
 
